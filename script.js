2e6a3deeaf6f23d27ae0c73b1cf62d0d9d1a9fe2
farhaanUbitto <47233425+farhaanUbitto@users.noreply.github.com>
2019-02-07 14:06:15 +0800

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ var SpeechGrammarList = SpeechGrammarList || webkitSpeechGrammarList;
 var SpeechRecognitionEvent = SpeechRecognitionEvent || webkitSpeechRecognitionEvent;
 
 var phrases = [
-  '소녀는 소년이다'
+  '소녀는 소년이다.'
 ];
 
 var phrasePara = document.querySelector('.phrase');
@@ -53,7 +53,7 @@ function testSpeech() {
     // These also have getters so they can be accessed like arrays.
     // The second [0] returns the SpeechRecognitionAlternative at position 0.
     // We then return the transcript property of the SpeechRecognitionAlternative object 
-    var speechResult = event.results[0][0].transcript;
+    var speechResult = event.results[0][0].transcript + '.';
     console.log(event.results[0][0].transcript);
     diagnosticPara.textContent = 'Speech received: ' + speechResult;
     if(speechResult === phrase) {
